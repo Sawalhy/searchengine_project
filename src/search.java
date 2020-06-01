df4302f1a9b3a57ab5e26f3f2a5d18ad1576d750
Sawalhy <Sawalhyahmed@gmail.com>
2020-06-01 02:25:45 +0200

    Saving before starting to integrate interface

--- src/search.java
+++ src/search.java
@@ -1,30 +1,41 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class search {
 
     Connection con;
+    String searchWord;
 
-    public void/*Array of strings corresponding to the*/ searchExcute() throws SQLException, ClassNotFoundException {
+    public search(String input) {
+        searchWord = input;
+    }
+
+    public ArrayList<String> searchExcute() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/searchindex?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         String user = "root";
         String pass = "";
-        con = DriverManager.getConnection(url,user,pass);
+        con = DriverManager.getConnection(url, user, pass);
 
-        if (con != null)
-        {
-
+        if (con != null) {
+            return ranker();
         }
-
+        return null;
     }
 
-    public void ranker() throws SQLException {
-        String sqlQuery = "SELECT * FROM `wordsindex`";
+    public ArrayList<String> ranker() throws SQLException {
+        String sqlQuery = "SELECT URL,(q2.TermFreq * q1.IDF) AS TFIDF,q2.HFreq,q2.InTitle FROM (SELECT * FROM `wordsindex` WHERE Word = \"" + searchWord + "\") q1 JOIN (SELECT * FROM wordsurlsindex WHERE TermFreq > 0) q2 ON q1.Word = q2.Word ORDER BY TFIDF DESC;";
         PreparedStatement statement = con.prepareStatement(sqlQuery);
         ResultSet set = statement.executeQuery();
-        set.next();
-        set.getString("Word");
+
+        ArrayList<String> result = new ArrayList<>();
+        int i = 0;
+        while (set.next()) {
+            result.add(i, set.getString("URL"));
+            i++;
+        }
+        return result;
+
+
     }
-
-
 }
--- src/searchServlet.java
+++ src/searchServlet.java
@@ -3,20 +3,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class searchServlet extends HttpServlet {
 
     String query;
-    String result;
+    ArrayList<String> result;
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String text = request.getParameter("mainBox");
 
+        String text = request.getParameter("mainBox");
         textProcessor tp = new textProcessor();
 
 
-        //ADD SQL INJECTION PROTECTOR
         if (text.charAt(0) == '"' && text.charAt(text.length()-1) == '"')
         {
             query = "ama nshoof han3mel elzeft dh ezai";
@@ -27,10 +27,10 @@ public class searchServlet extends HttpServlet {
             query = tp.removeStopwords(query);
         }
 
-        search srch = new search();
+        search srch = new search(query);
 
         try {
-            result = srch.searchExcute(query);
+            result = srch.searchExcute();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -40,7 +40,11 @@ public class searchServlet extends HttpServlet {
 
 /////////////////////////////////////
         response.setContentType("text/html");
-        String page = "<!doctype html> <html> <body> <h1>" + result +" </h1> </body></html>";
+        String page = "<!doctype html> <html> <body> <h1>";
+        for (int i = 0; i < result.size() ; i++) {
+            page = page + result.get(i) + "<br>";
+        }
+        page = page +  " </h1> </body></html>" ;
         response.getWriter().println(page);
     }
 
